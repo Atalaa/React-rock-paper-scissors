dcd441670de40e509b574a54070ee7603a4746cb
Atala <atala.rchidi@gmail.com>
2022-10-28 14:42:01 -0400

    added countdown timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import {
   FaRegHandRock,
@@ -10,43 +10,49 @@ import styles from "./App.module.css";
 function App() {
   const [playerHand, setPlayerHand] = useState(0);
   const [computerHand, setComputerHand] = useState(0);
+  const [timer, setTimer] = useState(3);
+  const [runTimer, setRunTimer] = useState(false);
+  const [score, setScore] = useState(0);
+
   const icons = [
     { name: "rock", icon: <FaRegHandRock color="black" size={60} /> },
     { name: "paper", icon: <FaRegHandPaper color="black" size={60} /> },
     { name: "scissors", icon: <FaRegHandScissors color="black" size={60} /> },
   ];
 
-  const [noneHand, setNoneHand] = useState(false);
-  const [score, setScore] = useState(0);
+  useEffect(() => {
+    if (runTimer && timer > 0) {
+      setTimeout(() => {
+        setTimer((timer) => timer - 1);
+      }, 1000);
+    } else if (runTimer && timer < 1) {
+      setRunTimer(false);
+      setTimer(3);
+    }
+  }, [runTimer, timer]);
 
-  //human player
   const selectHand = (handIndex) => {
     setPlayerHand(handIndex);
   };
 
-  //computer
   const generateComputerHand = () => {
     const randomeNumber = Math.floor(Math.random() * 3);
     setComputerHand(randomeNumber);
-    setNoneHand(true);
   };
 
   const play = () => {
+    setRunTimer(true);
     generateComputerHand();
   };
 
-  console.log("playerHand:", playerHand);
-  console.log("computerHand:", computerHand);
-
   return (
     <main className={styles.container}>
-      {/* title page */}
       <div className={styles.titleContainer}>
         <h1>ROCK, PAPER, SCISSORS</h1>
         <p>Enjoy the game!</p>
       </div>
 
-      {/* score */}
+      {/* SCORE */}
       <div className={styles.scoreContainer}>
         <div className={styles.score}>
           <h3>Player</h3>
@@ -68,8 +74,13 @@ function App() {
 
         {/* MESSAGE WINNER */}
         <div className={styles.midCol}>
-          <p className={styles.resultsWinner}>Winner: Player</p>
-          <p className={styles.resultsMessage}>Rock beats Scissors</p>
+          {runTimer && <p className={styles.timer}>{timer}</p>}
+          {/* {!runTimer && (
+            <div>
+              <p className={styles.resultsWinner}>Winner: Player</p>
+              <p className={styles.resultsMessage}>Rock beats Scissors</p>
+            </div>
+          )} */}
         </div>
 
         {/* COMPUTER */}
@@ -81,43 +92,43 @@ function App() {
         </div>
       </div>
 
-      {/* buttons choice */}
+      {/* BUTTONS CHOICE */}
       <div className={styles.choiceBtnContainer}>
-        {/* Rock button*/}
+        {/* ROCK */}
         <button
           className={`${styles.choiceBtn} ${styles.bounce} ${
             playerHand === 0 ? styles.activeHand : ""
           }`}
           onClick={() => selectHand(0)}
         >
-          <FaRegHandRock color="black" size={60} />
-          Rock
+          {icons[0].icon}
+          {icons[0].name}
         </button>
 
-        {/* Paper button*/}
+        {/* PAPER */}
         <button
           className={`${styles.choiceBtn} ${styles.bounce} ${
             playerHand === 1 ? styles.activeHand : ""
           }`}
           onClick={() => selectHand(1)}
         >
-          <FaRegHandPaper color="black" size={60} />
-          Paper
+          {icons[1].icon}
+          {icons[1].name}
         </button>
 
-        {/* Scissors button*/}
+        {/* SCISSORS */}
         <button
           className={`${styles.choiceBtn} ${styles.bounce} ${
             playerHand === 2 ? styles.activeHand : ""
           }`}
           onClick={() => selectHand(2)}
         >
-          <FaRegHandScissors color="black" size={60} />
-          Scissors
+          {icons[2].icon}
+          {icons[2].name}
         </button>
       </div>
 
-      {/* Play button */}
+      {/* PLAY */}
       <button className={styles.playBtn} onClick={play}>
         Play
       </button>
