149b8240edcdda4d9a1851ef5962bdc52b8bbd04
Atala <atala.rchidi@gmail.com>
2022-10-28 11:14:02 -0400

    displayed selected hand

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,14 @@ import styles from "./App.module.css";
 function App() {
   const [playerHand, setPlayerHand] = useState(0);
   const [computerHand, setComputerHand] = useState(0);
+  const icons = [
+    { name: "rock", icon: <FaRegHandRock color="black" size={60} /> },
+    { name: "paper", icon: <FaRegHandPaper color="black" size={60} /> },
+    { name: "scissors", icon: <FaRegHandScissors color="black" size={60} /> },
+  ];
+
+  const [noneHand, setNoneHand] = useState(false);
+  const [score, setScore] = useState(0);
 
   //human player
   const selectHand = (handIndex) => {
@@ -20,6 +28,7 @@ function App() {
   const generateComputerHand = () => {
     const randomeNumber = Math.floor(Math.random() * 3);
     setComputerHand(randomeNumber);
+    setNoneHand(true);
   };
 
   const play = () => {
@@ -30,39 +39,51 @@ function App() {
   console.log("computerHand:", computerHand);
 
   return (
-    <div className={styles.container}>
+    <main className={styles.container}>
+      {/* title page */}
       <div className={styles.titleContainer}>
         <h1>ROCK, PAPER, SCISSORS</h1>
         <p>Enjoy the game!</p>
       </div>
+
       {/* score */}
       <div className={styles.scoreContainer}>
         <div className={styles.score}>
           <h3>Player</h3>
-          <p>Score: 0</p>
+          <p>Score: {score}</p>
         </div>
         <div className={styles.score}>
           <h3>Computer</h3>
-          <p>Score: 0</p>
+          <p>Score: {score}</p>
         </div>
       </div>
-      {/* results */}
+
+      {/* RESULTS */}
       <div className={styles.resultsContainer}>
+        {/* HUMAN PLAYER */}
         <div className={styles.playerHand}>
-          <FaRegHandRock color="black" size={60} />
-          <p>Rock</p>
+          {icons[playerHand].icon}
+          <p>{icons[playerHand].name}</p>
         </div>
+
+        {/* MESSAGE WINNER */}
         <div className={styles.midCol}>
           <p className={styles.resultsWinner}>Winner: Player</p>
           <p className={styles.resultsMessage}>Rock beats Scissors</p>
         </div>
+
+        {/* COMPUTER */}
         <div className={styles.computerHand}>
-          <FaRegHandPaper color="black" size={60} />
-          <p>Scissor</p>
+          <div>
+            {icons[computerHand].icon}
+            <p>{icons[computerHand].name}</p>
+          </div>
         </div>
       </div>
+
       {/* buttons choice */}
       <div className={styles.choiceBtnContainer}>
+        {/* Rock button*/}
         <button
           className={`${styles.choiceBtn} ${styles.bounce} ${
             playerHand === 0 ? styles.activeHand : ""
@@ -72,6 +93,8 @@ function App() {
           <FaRegHandRock color="black" size={60} />
           Rock
         </button>
+
+        {/* Paper button*/}
         <button
           className={`${styles.choiceBtn} ${styles.bounce} ${
             playerHand === 1 ? styles.activeHand : ""
@@ -81,6 +104,8 @@ function App() {
           <FaRegHandPaper color="black" size={60} />
           Paper
         </button>
+
+        {/* Scissors button*/}
         <button
           className={`${styles.choiceBtn} ${styles.bounce} ${
             playerHand === 2 ? styles.activeHand : ""
@@ -91,10 +116,12 @@ function App() {
           Scissors
         </button>
       </div>
+
+      {/* Play button */}
       <button className={styles.playBtn} onClick={play}>
         Play
       </button>
-    </div>
+    </main>
   );
 }
 
